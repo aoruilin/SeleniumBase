385a3bfa0ca94cb48407dd2e2a7f20797d29dc21
aoruilin0208 <646010544@qq.com>
2020-01-06 11:50:55 +0800

    ①更新作品大厅学生发布作品的操作和用例

--- ui_auto/testcase/testsuits/run_all_tests.py
+++ ui_auto/testcase/testsuits/run_all_tests.py
@@ -1,7 +1,7 @@
 from ui_auto.testcase.edu_main_process import TestMainProcess
 from ui_auto.testcase.packages_course import TestPackageCourse
 from ui_auto.testcase.homework_loop import HomeworkLoop
-# from ui_auto.testcase.test_field_operation import TestFieldCase
+from ui_auto.testcase.test_field_operation import TestFieldCase
 from ui_auto.testcase.my_creation_work import TestMyCreation
 from ui_auto.testcase.test_field_work import WorksHall
 from ui_auto.testcase.user_login_wrong import UserLoginWrong
